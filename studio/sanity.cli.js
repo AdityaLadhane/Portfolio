50d316500d3dacb9dd45b66b1915e192f6e0bba4
✶Aditya✶ <112150367+AdityaLadhane@users.noreply.github.com>
2026-01-29 15:39:32 +0530

    Add Sanity Studio deployment app ID

--- studio/sanity.cli.js
+++ studio/sanity.cli.js
@@ -6,6 +6,7 @@ export default defineCliConfig({
     dataset: 'production'
   },
   deployment: {
+    appId: 'mtg4u1zvksfa3hnl6wf3ua22',
     /**
      * Enable auto-updates for studios.
      * Learn more at https://www.sanity.io/docs/studio/latest-version-of-sanity#k47faf43faf56
